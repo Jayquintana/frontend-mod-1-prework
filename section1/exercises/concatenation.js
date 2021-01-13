f111586841179d10a3516e347c1150d2af632849
Jacob Quintana <jacobquintaa@gmail.com>
2021-01-12 20:51:27 -0700

    Add Section 1

--- section1/exercises/concatenation.js
+++ section1/exercises/concatenation.js
@@ -19,11 +19,13 @@ console.log("The " + team + " are " + name + "'s favorite Quidditch team");
 var numberOfCreatures = 7;
 var creatures = "unicorns";
 
-console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
+console.log("My zoo has " + numberOfCreatures + " colorful " + creatures + "!");
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
-// Your answer and explanation here: 
+// Your answer and explanation here:
 
+// A string data type will be logged ("My zoo has 7 colorful unicorns") as a result because it
+// converts number of creatures as a string to be read and combined called coercion
 
 // YOU DO:
 // Write code that combines the variables below into a string that
@@ -31,7 +33,7 @@ console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
 var speedy = "quick red fox";
 var slowPoke = "lazy brown dog";
 
-
+console.log("The " + speedy + " jumped over " + slowPoke);
 // YOU DO:
 // Write code that combines the variables below into a string that
 // reads "In a predictable result, the tortoise beat the hare!"
@@ -40,16 +42,22 @@ var slowPoke = "lazy brown dog";
 slowPoke = "tortoise";
 speedy = "hare";
 
+console.log("In a predictable result, the " + slowPoke + " beat the " + speedy + "!")
 
 // YOU DO:
-// Declare three variables, name/content/data type of your choice. Think carefully about what 
+// Declare three variables, name/content/data type of your choice. Think carefully about what
   // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
   // Then, log out ONE sentence that incorporates all THREE variables.
+var badMovie = "Twilight is the worst"
+var worseMovie = "I liked the Joker instead"
+var timesWatched = 22
+
+console.log("Man,the movie " + badMovie + ". " + worseMovie + "." + " I saw it " + timesWatched + " times!")
 
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
+// Did you run this file in your terminal to make sure everything printed out to the console
   // as you would expect? You should be doing this by now, and getting into the habit of it.
